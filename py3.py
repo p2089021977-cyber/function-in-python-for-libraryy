c26d0de893ae58b7a74cdc11bdc37eb4aa9d4291
p2089021977-cyber <p2089021977@gmail.com>
2025-11-12 11:20:31 +0530

    Add files via upload

---
+++ py3.py
@@ -0,0 +1,21 @@
+print("welcome")
+def run():
+      print("1 for continue")
+      print("2 for exist")
+      a=int(input("press 1 for continue and 2 for exist"))
+      if a==1:
+          pass
+      elif a==2: 
+         print("thanks for visit")
+def book():
+    print("press 1 for book1:value of education")
+    print("press 1 for book1")
+    print("press 1 for book1")
+    print("press 1 for book1")
+    b=int(input("enter book number to continue"))
+    if b==1:
+       print("value of education:--The value of education lies in its ability to develop well-rounded individuals who are not only academically knowledgeable but ")
+       run()
+    elif b==2:
+        print("book2:--description")
+book()
